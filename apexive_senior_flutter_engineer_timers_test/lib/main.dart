0cbf00008622c286845113e3c0e264f8232634c9
Alexander <m.mirvua@gmail.com>
2024-04-06 20:59:56 +0300

    finished top section;

--- apexive_senior_flutter_engineer_timers_test/lib/main.dart
+++ apexive_senior_flutter_engineer_timers_test/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:apexive_senior_flutter_engineer_timers_test/ui/my_app_bar.dart';
+import 'package:apexive_senior_flutter_engineer_timers_test/ui/typography.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,14 +13,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Odoo',
       theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          toolbarHeight: 64,
-          titleTextStyle: TextStyle(
-            fontSize: 32,
-            color: Colors.white,
-            fontWeight: FontWeight.w700,
-          )
-        ),
+        // appBarTheme: AppBarTheme(
+        //   toolbarHeight: 64,
+        //   titleTextStyle: TextStyle(
+        //     fontSize: 32,
+        //     color: Colors.white,
+        //     fontWeight: FontWeight.w700,
+        //   )
+        // ),
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Timesheets'),
@@ -49,59 +49,97 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        titleSpacing: 24,
+      backgroundColor: Colors.indigoAccent,
+      //extendBodyBehindAppBar: true,
+      // appBar: AppBar(
+      //   title: Text(widget.title),
+      //   backgroundColor: Colors.transparent,
+      //   elevation: 0.0,
+      //   titleSpacing: 24,
+      //
+      //   actions: [
+      //     Container(
+      //       margin: const EdgeInsets.only(top: 8, bottom: 8, right: 16),
+      //       child: Center(
+      //         child: Ink(
+      //           //height: 48,
+      //           padding: EdgeInsets.all(8),
+      //           //width: 48,
+      //           decoration: BoxDecoration(
+      //             borderRadius: BorderRadius.circular(15),
+      //             color: const Color(0x29ffffff),
+      //           ),
+      //           child: IconButton(
+      //             onPressed: onAddTimerButtonPressed,
+      //             icon: const Icon(Icons.add, size: 24, color: Colors.white,),
+      //             //color: const Color(0x29ffffff),
+      //           ),
+      //         ),
+      //       ),
+      //     )
+      //   ],
+      // ),
+      body: SafeArea(
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter, // Start direction
+              end: Alignment.bottomCenter, // End direction
+              colors: [
+                Color(0xff1c2658), // Start Color
+                Color(0xff364fc7),// End Color
+              ], // Customize your colors here
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(right: 16, left: 16),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 8, bottom: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    //crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text('Timesheets', style: TypographyStyles.headlineLarge,),
 
-        actions: [
-          Container(
-            margin: EdgeInsets.only(top: 8, bottom: 8, right: 16),
-            child: Center(
-              child: Ink(
-                //height: 48,
-                padding: EdgeInsets.all(8),
-                //width: 48,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: const Color(0x29ffffff),
-                ),
-                child: IconButton(
-                  onPressed: onAddTimerButtonPressed,
-                  icon: const Icon(Icons.add, size: 24, color: Colors.white,),
-                  //color: const Color(0x29ffffff),
-                ),
-              ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        child: Row(
+                          children: [
+
+                            Container(
+                              height: 48,
+                              width: 48,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(12),
+                                  color: const Color(0x29ffffff)
+                              ),
+                              padding: EdgeInsets.all(8.0),
+                              child: const Icon(Icons.sync_alt, color: Colors.white),
+                            ),
+
+                            Padding(padding: EdgeInsets.only(right: 8)),
+
+                            Container(
+                              height: 48,
+                              width: 48,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12),
+                                color: const Color(0x29ffffff)
+                              ),
+                              padding: EdgeInsets.all(8.0),
+                              child: const Icon(Icons.add, color: Colors.white,),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
             ),
           )
-        ],
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter, // Start direction
-            end: Alignment.bottomCenter, // End direction
-            colors: [
-              Color(0xff1c2658), // Start Color
-              Color(0xff364fc7),// End Color
-            ], // Customize your colors here
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ],
-          ),
         ),
       ),
       // floatingActionButton: FloatingActionButton(
---
+++ apexive_senior_flutter_engineer_timers_test/lib/ui/typography.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TypographyStyles {
+
+  static const TextStyle headlineLarge = TextStyle(fontSize: 32,
+                                                   fontWeight: FontWeight.w700,
+                                                   height: 1.4,
+                                                   color: Colors.white);
+
+
+}
