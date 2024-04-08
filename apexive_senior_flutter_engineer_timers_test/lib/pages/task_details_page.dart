b47db9dc8c987ca75228360dd3be01261931b39c
Alexander <m.mirvua@gmail.com>
2024-04-08 15:46:59 +0300

    started working on task details page;

---
+++ apexive_senior_flutter_engineer_timers_test/lib/pages/task_details_page.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class TaskDetailsPage extends StatefulWidget {
+  const TaskDetailsPage({super.key});
+
+  static const routeName = '/TaskDetailsActivity';
+
+  @override
+  State<TaskDetailsPage> createState() => _TaskDetailsPageState();
+}
+
+class _TaskDetailsPageState extends State<TaskDetailsPage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container();
+  }
+
+}
