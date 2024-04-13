cbc6b3ab337fa5c040ee1026748074b242fb7756
Alexander <m.mirvua@gmail.com>
2024-04-13 20:58:59 +0300

    minor changes;

--- apexive_senior_flutter_engineer_timers_test/lib/model/time_sheet_item.dart
+++ apexive_senior_flutter_engineer_timers_test/lib/model/time_sheet_item.dart
@@ -4,8 +4,7 @@ import 'dart:async';
 import 'package:apexive_senior_flutter_engineer_timers_test/model/project.dart';
 import 'package:apexive_senior_flutter_engineer_timers_test/model/task.dart';
 
-//substitute for a timer
-//but the timer name is already taken by the OS;
+//substitute for a timer class for the name is already taken by the OS;
 
 class TimeSheetItem {
   final String description;
